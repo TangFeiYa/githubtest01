634ba441025dcd03546dbe870923f53508dea6c3
tangfei <1446998864@qq.com>
2021-12-21 19:10:02 +0800

    c1客户端操作

--- src/main/java/com/oracle/test/testoo2.java
+++ src/main/java/com/oracle/test/testoo2.java
@@ -2,6 +2,6 @@ package com.oracle.test;
 
 public class testoo2 {
     public static void main(String[] args) {
-        System.out.println("testoo2");
+        System.out.println("c1客户端testoo2");
     }
 }
